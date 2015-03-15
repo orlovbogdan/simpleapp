e403ee433af3e83854a4cf7e0a5eaee3a88e88da
Bogdan <b.orlov.fl@yandex.ru>
2015-03-15 23:04:11 +0300

    add dinamic content to liquid

--- app/models/page.rb
+++ app/models/page.rb
@@ -7,6 +7,12 @@ class Page < ActiveRecord::Base
 
   before_validation :generate_slug
 
+  liquid_methods :name, :content, :slug, :created_at, :pages
+
+  def pages
+    Page.all
+  end
+
   def to_param
     slug
   end
