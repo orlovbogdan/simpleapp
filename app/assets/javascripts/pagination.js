235135e63d7af248987c1dbd95297c0c9f03907f
Bogdan <b.orlov.fl@yandex.ru>
2015-03-23 01:05:11 +0300

    add pushstate feature

--- app/assets/javascripts/pagination.js
+++ app/assets/javascripts/pagination.js
@@ -1,24 +1,33 @@
-$(function(){
-  $(document).on('click','.pagination a, th a',function(){
-    $(".pagination").html("Page is loading...");
-    $.get(this.href, null, null, 'script');
-    return false;
-  });
-  /*$(document).on('submit','#search_form', function(){
-    $.get(this.action, $(this).serialize(), null, 'script');
-    return false;
-  })*/
-  $(document).on('input','#search_form input', function() {
-    $.get($('#search_form').attr('action'), $('#search_form').serialize(), null, 'script');
-    return false;
-  });
-
-  $(window).scroll(function(){
-    var url = $('.pagination .next a').attr('href');
-    if (url && $(window).scrollTop() > $(document).height() - $(window).height() - 50){
+if (history && history.pushState) {
+  $(function () {
+    $(document).on('click', '.pagination a, th a', function (e) {
       $(".pagination").html("Page is loading...");
-      $.getScript(url + '?endless=true');
-    }
-  })
+      $.get(this.href, null, null, 'script');
+      history.pushState(null, document.title, this.href);
+      //e.preventDefault();
+      return false;
+    });
+    /*$(document).on('submit','#search_form', function(){
+     $.get(this.action, $(this).serialize(), null, 'script');
+     return false;
+     })*/
+    $(document).on('input', '#search_form input', function () {
+      $.get($('#search_form').attr('action'), $('#search_form').serialize(), null, 'script');
+      history.replaceState(null, document.title, $('#search_form').attr('action') + "?" + $('#search_form').serialize());
+      return false;
+    });
 
-})
+    $(window).bind("popstate", function() {
+      $.getScript(location.href);
+    });
+
+    $(window).scroll(function () {
+      var url = $('.pagination .next a').attr('href');
+      if (url && $(window).scrollTop() > $(document).height() - $(window).height() - 50) {
+        $(".pagination").html("Page is loading...");
+        $.getScript(url + '?endless=true');
+      }
+    })
+
+  })
+}
