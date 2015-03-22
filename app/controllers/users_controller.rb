4a69f7acf668dc434a004be3fc86a6bef12c9d7c
Bogdan <b.orlov.fl@yandex.ru>
2015-03-22 11:44:34 +0300

    fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     @user = params[:user] ? User.new(users_params) : User.new_guest
     if @user.save
       current_user.move_to(@user) if current_user && current_user.guest?
-      session[:user_id] = @user.id
+      #session[:user_id] = @user.id
       redirect_to root_url, :notice => "Signed up!"
     else
       render "new"
